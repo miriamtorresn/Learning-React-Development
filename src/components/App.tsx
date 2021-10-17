5b84e4b7d5805944740ae45f28f201d20bc53b87
Miriam Torres <miriamtorres.navarro@gmail.com>
2021-10-17 11:51:54 -0500

    React cleaned.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
-import HelloWorld from './hello-world';
-
 function App() {
   return (
     <div className="App">
-      <HelloWorld name="Luis"></HelloWorld>
+      Hola
     </div>
   );
 }
