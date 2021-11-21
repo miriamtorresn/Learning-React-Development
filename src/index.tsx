647ca9e806fbe286cf1f87f18d1807f8915cd56a
Miriam Torres <miriamtorres.navarro@gmail.com>
2021-11-21 14:03:29 -0600

    Configuring redux.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import initialState from './redux/initialState';
+import configureStore from './redux/configureStore';
+import { Provider as ReduxProvider } from "react-redux";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const store = configureStore(initialState);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ReduxProvider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ReduxProvider>,
   document.getElementById('root')
 );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 // npm install axios --save
 import axios from 'axios';
 import { Container, Row, Col } from 'react-bootstrap';
@@ -9,25 +11,36 @@ import CartSummary from '../components/CartSummary';
 import MainMenu from '../components/MainMenu';
 import PaginationNav from '../components/PaginationNav';
 import { IProduct } from '../interfaces/Product';
+import { IStore } from '../redux/initialState';
+import { updateProducts, IUpdateProductsAction } from '../redux/actions/productActions';
 
+interface IHomePageProps {
+  products: IProduct[];
+  updateProducts: (products: IProduct[]) => IUpdateProductsAction;
+}
 
-class HomePage extends React.Component {
+class HomePage extends React.Component<IHomePageProps> {
   state = {
-    products: [],
     cartItems: []
   };
 
   componentDidMount() {
-    axios.get('https://61101b8dc848c900171b3a84.mockapi.io/products')
-      .then((res: any) => {
-        const products: IProduct[] = res.data;
-        products.map((product: IProduct) => {
-          product.quantity = 0;
-          return product;
-        });
+    const { products, updateProducts } = this.props;
+    // If products don't exist in redux, I should make my api call
+    // and update products in redux.
 
-        this.setState({ products });
+    if (products.length === 0) {
+      axios.get('https://61101b8dc848c900171b3a84.mockapi.io/products')
+        .then((res: any) => {
+          const _products: IProduct[] = res.data
+            .map((product: IProduct) => {
+              product.quantity = 0;
+              return product;
+            });
+          // update products in redux
+          updateProducts(_products);
       });
+    }
   }
 
   addToCart = (product: IProduct): void => {
@@ -41,15 +54,17 @@ class HomePage extends React.Component {
   };
 
   updateProduct = (product: IProduct) => {
-    const products: IProduct[] = this.state.products.map((_product: IProduct) => {
-      if (product.id === _product.id) {
-        _product = product;
-      }
-      return _product;
-    });
-    const cartItems = products.filter((product: IProduct) => product.quantity > 0);
-    this.setState({ cartItems });
-    this.setState({ products });
+    // Commenting to migrate to redux.
+    console.log('trying to update', product);
+    // const products: IProduct[] = this.state.products.map((_product: IProduct) => {
+    //   if (product.id === _product.id) {
+    //     _product = product;
+    //   }
+    //   return _product;
+    // });
+    // const cartItems = products.filter((product: IProduct) => product.quantity > 0);
+    // this.setState({ cartItems });
+    // this.setState({ products });
   };
 
   getCartItemsTotal = (): number => {
@@ -57,6 +72,8 @@ class HomePage extends React.Component {
   };
 
   render() {
+    const { products } = this.props;
+    const { cartItems } = this.state;
     return (
       <Container>
         <Row>
@@ -68,11 +85,11 @@ class HomePage extends React.Component {
         </Row>
         <Row>
           <h2>Cart Summary</h2>
-          <CartSummary cartItems={this.state.cartItems} />
+          <CartSummary cartItems={cartItems} />
         </Row>
         <Row>
           <h2>Products</h2>
-          <ProductCardList products={this.state.products} addToCart={this.addToCart} removeFromCart={this.removeFromCart}/>
+          <ProductCardList products={products} addToCart={this.addToCart} removeFromCart={this.removeFromCart}/>
         </Row>
         <Row>
           <Col>
@@ -83,5 +100,12 @@ class HomePage extends React.Component {
     );
   }
 }
+const mapStateToProps = (state: IStore) => ({
+  products: state.productReducer.products
+});
 
-export default HomePage;
+const mapDispatchToProps = (dispatch: any) => ({
+  updateProducts: bindActionCreators(updateProducts, dispatch)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
